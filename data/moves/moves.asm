7d3d1c7d1b603f8cec3db604bff5bb63872d99b4
c-crescent <lightredemption@hotmail.com>
2026-01-30 16:00:57 +0700

    Buffed Razor Wind

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -26,7 +26,7 @@ Moves:
 	move SCRATCH,      EFFECT_NORMAL_HIT,         40, NORMAL,       100, 35,   0
 	move VICEGRIP,     EFFECT_NORMAL_HIT,         90, NORMAL,       100, 30,   0
 	move GUILLOTINE,   EFFECT_OHKO,                0, NORMAL,        30,  5,   0
-	move RAZOR_WIND,   EFFECT_RAZOR_WIND,         80, NORMAL,       100, 15,   0
+	move RAZOR_WIND,   EFFECT_RAZOR_WIND,        100, NORMAL,       100, 10,   0
 	move SWORDS_DANCE, EFFECT_ATTACK_UP_2,         0, NORMAL,       100, 30,   0
 	move CUT,          EFFECT_NORMAL_HIT,         55, BUG,          100,  30,   0
 	move GUST,         EFFECT_GUST,               40, FLYING,       100, 35,   0
